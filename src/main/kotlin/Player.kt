02481ff6d1ce5ddf4d4c785f9b8b8002eec69f15
Michał <michaloginski15@gmail.com>
2024-06-05 19:35:43 +0200

    Init.

---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,12 @@
+﻿class Player(val name:String, private val sign:String) 
+{
+    fun GetSign():String
+    {
+        return sign
+    }
+    
+    fun GetName():String
+    {
+        return name
+    }
+}
